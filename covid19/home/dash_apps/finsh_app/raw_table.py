1ced7a273df3ca248a81920afb1b5179a3a02ec4
waikato_sa252 <sa252@waikato.students.ac.nz>
2020-05-25 22:05:12 +1200

    update raw table

--- covid19/home/dash_apps/finsh_app/raw_table.py
+++ covid19/home/dash_apps/finsh_app/raw_table.py
@@ -1,17 +1,17 @@
-import dash_table
-from django_plotly_dash import DjangoDash
-import pandas as pd
-
-df = pd.read_csv('corona.csv')
-app = DjangoDash('raw_table')
-app.layout = dash_table.DataTable(
-    data=df.to_dict('records'),
-    columns=[
-        {'name': i, 'id': i} for i in df.columns
-    ],
-    style_data_conditional=[{
-        'if': {'column_id': 'TotalCases'},
-        'backgroundColor': '#3D9970',
-        'color': 'white',
-    }]
-)
+# import dash_table
+# from django_plotly_dash import DjangoDash
+# import pandas as pd
+#
+# df = pd.read_csv('corona.csv')
+# app = DjangoDash('raw_table')
+# app.layout = dash_table.DataTable(
+#     data=df.to_dict('records'),
+#     columns=[
+#         {'name': i, 'id': i} for i in df.columns
+#     ],
+#     style_data_conditional=[{
+#         'if': {'column_id': 'TotalCases'},
+#         'backgroundColor': '#3D9970',
+#         'color': 'white',
+#     }]
+# )
